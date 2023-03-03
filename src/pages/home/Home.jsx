967fa16feb8953da1b3024b765a4060196ac411d
DANNJUGUNA <ndan30071@gmail.com>
2023-03-03 09:28:15 +0300

    updated the code and the css rules

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import TopBar from '../../components/topbar/TopBar'
+import"./home.css"
+const Home = () => {
+  return (
+    <>
+    
+    <div className="homepagecontent">
+        <p >
+            WELCOME
+        </p>
+        <p className='p1'>
+            TO
+        </p>
+        <p>
+            RECIPE-SHARING-APPLICATION
+        </p>
+    </div>
+    </>
+  )
+}
+
+export default Home
