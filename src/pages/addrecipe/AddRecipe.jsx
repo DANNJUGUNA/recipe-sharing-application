caf88ba79a8a4164c37226d7039efbaec4f2df74
DANNJUGUNA <ndan30071@gmail.com>
2023-03-03 09:26:35 +0300

    created the add recipe page and its css

---
+++ src/pages/addrecipe/AddRecipe.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import RecipeForm from '../../components/recipe form/RecipeForm'
+
+import"./addrecipe.css"
+const AddRecipe = () => {
+  return (
+    <>
+  
+    <div className='recipe1'>
+        <RecipeForm/>
+    </div>
+    </>
+  )
+}
+
+export default AddRecipe
