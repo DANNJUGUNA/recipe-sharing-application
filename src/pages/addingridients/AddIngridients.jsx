31d8af02de6f57d54ffaa1be58ee045ef454eba2
DANNJUGUNA <ndan30071@gmail.com>
2023-03-03 09:27:32 +0300

    created the page and its css

---
+++ src/pages/addingridients/AddIngridients.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import IngridientForm from '../../components/ingridientForm/IngridientForm'
+
+import"./add.css"
+const AddIngridients = () => {
+  return (
+    <>
+    
+    <div className="addingridientsform">
+        <IngridientForm/>
+    </div>
+    </>
+  )
+}
+
+export default AddIngridients
