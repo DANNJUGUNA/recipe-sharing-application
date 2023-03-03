75e6965fbf1a0cd971ea2094761c621acced6e41
DANNJUGUNA <ndan30071@gmail.com>
2023-03-03 09:28:39 +0300

    created the page and its css

---
+++ src/pages/ingridients/Ingridients.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Card from '../../components/card1/Card1'
+import TopBar from '../../components/topbar/TopBar'
+import { useEffect, useState } from 'react'
+
+const Ingridients = () => {
+    const[ingridients,setingridients]=useState([])
+    useEffect(()=>{
+      fetch("http://localhost:9292/ingridients")
+      .then((response)=>response.json())
+    .then((data)=>setingridients(data))
+  },[])
+  return (
+   <>
+   
+   <div>
+    {
+      ingridients.map((ingridient)=>{
+       return(
+        <Card key={ingridient.id} id={ingridient.id} ingridient={ingridient}/>
+       )
+      })
+    }
+   </div>
+   </>
+  )
+}
+
+export default Ingridients
