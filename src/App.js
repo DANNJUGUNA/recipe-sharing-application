dec483953daca35f1dd424280c3adbc1a9d5b994
DANNJUGUNA <ndan30071@gmail.com>
2023-03-03 09:23:50 +0300

    added routes to the application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import TopBar from "./components/topbar/TopBar";
+import AddIngridients from "./pages/addingridients/AddIngridients";
+import AddRecipe from "./pages/addrecipe/AddRecipe";
+import Home from "./pages/home/Home";
+import Ingridients from "./pages/ingridients/Ingridients";
+import Recipes from "./pages/recipes/Recipes";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+    <TopBar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/recipe" element={<Recipes/>}/>
+      <Route path="/addrecipes" element={<AddRecipe/>}/>
+      <Route path="/ingridients" element={<Ingridients/>}/>
+      <Route path="/addingridients" element={<AddIngridients/>}/>
+    </Routes>
     </div>
+
   );
 }
 
